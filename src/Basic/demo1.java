0b0b85562b29c85a477ec12e65c6804bd34769f3
datta <datta@dattachalwad>
2023-02-06 16:30:33 +0530

    First commit-1

---
+++ src/Basic/demo1.java
@@ -0,0 +1,20 @@
+package Basic;
+
+public class demo1 
+{
+
+	public static void main(String[]args)
+	{
+		
+		System.out.println("welcome");
+		m1();
+	}
+	
+
+ 
+ public static void m1()
+ {
+	 System.out.println("Hello");	 
+ }
+ 
+}
